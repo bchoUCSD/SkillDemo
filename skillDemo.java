2e172d96d9b120ccb12ce57568ce2f07db5a3d09
bchoUCSD <bcho@ucsd.edu>
2022-04-28 14:57:20 -0700

    create skillDemo

--- skillDemo.java
+++ skillDemo.java
@@ -1,4 +1,13 @@
 class skillDemo
 {
+    private static int[] arr = {1,2};
 
+    public static int getAt(int index)
+    {
+        if(index > arr.length-1 && index > -1)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        return arr[index];
+    }
 }
---
+++ skillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTester {
+    @Test
+    public void getAtTest()
+    {
+        assertEquals(1,skillDemo.getAt(1));
+    }
+}   
